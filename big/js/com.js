765468a169905c491d4bab44bf23257ccc0f56c4
jialufeng <1014142434@qq.com>
2016-09-22 11:32:16 +0800

    big

---
+++ big/js/com.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready(function() {
+
+    var Height = $(window).height();
+
+    $('#particles').height(Height);
+
+
+    $(window).scroll(function() {
+        console.log(($(window).scrollTop() + 200) > $('.rotate-img').offset().top)
+        if(($(window).scrollTop() + 200) > $('.rotate-img').offset().top) {
+            $('.rotate-img').addClass('trans')
+        } else {
+            $('.rotate-img').removeClass('trans')
+
+        }
+    })
+
+    $('.intr_auto a').on('click', function() {
+        console.log($('.intr_auto a').index($(this)));
+
+        $(this).addClass('active').siblings().removeClass('active');
+        var index = $(this).index();
+        console.log(index);
+        $('.tabs-pm > div').eq(index).show().siblings().hide();
+    })
+
+});
