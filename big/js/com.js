9372d467c06725733d4bcd1217f1842356b91822
jialufeng <1014142434@qq.com>
2016-10-14 12:17:28 +0800

    add

--- big/js/com.js
+++ big/js/com.js
@@ -14,24 +14,14 @@ $(document).ready(function() {
     });
 
 
-    $(window).scroll(function() {
-        console.log(($(window).scrollTop() + 200) > $('.rotate-img').offset().top)
-        if(($(window).scrollTop() + 200) > $('.rotate-img').offset().top) {
-            $('.rotate-img').addClass('trans')
-        } else {
-            $('.rotate-img').removeClass('trans')
-
-        }
-    })
-
-    $('.intr_auto a').on('click', function() {
-        console.log($('.intr_auto a').index($(this)));
-
-        $(this).addClass('active').siblings().removeClass('active');
-        var index = $(this).index();
-        console.log(index);
-        $('.tabs-pm > div').eq(index).show().siblings().hide();
-    })
+    //$('.intr_auto a').on('click', function() {
+    //    console.log($('.intr_auto a').index($(this)));
+    //
+    //    $(this).addClass('active').siblings().removeClass('active');
+    //    var index = $(this).index();
+    //    console.log(index);
+    //    $('.tabs-pm > div').eq(index).show().siblings().hide();
+    //})
 
 
 
